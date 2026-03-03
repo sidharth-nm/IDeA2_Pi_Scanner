0dc4c8af5fbf7add289a5a426ab8413829645ce9
sidharth-nm <sidharth.nimmagadda@gmail.com>
2026-03-03 18:10:56 +0800

    minor fix + some comments

--- upload_to_cad_instructions.py
+++ upload_to_cad_instructions.py
@@ -6,14 +6,20 @@ Unzips the file from the previous script, extracts the .OBJ file and uploads to 
 import os
 import zipfile
 import requests
+from dotenv import load_dotenv
 
+# Configuration; directory/file paths and loading webhook URL from env
+load_dotenv()
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 script_dir = os.path.dirname(os.path.abspath(__file__))
 zip_path = os.path.join(script_dir, "output.zip")
 extract_path = os.path.join(script_dir, "output")
 
+# Extracts the zip file to a folder within current directory
 with zipfile.ZipFile(zip_path, "r") as zip_ref:
     zip_ref.extractall(extract_path)
 
+# Finds & stores the OBJ file to be uploaded to n8n
 obj_file = next(
     (os.path.join(root, f)
      for root, _, files in os.walk(extract_path)
@@ -21,13 +27,15 @@ obj_file = next(
     None
 )
 
+# If no OBJ file found, throws an error
 if not obj_file:
     raise FileNotFoundError("No .OBJ file found in output folder")
 
 print(f"Found OBJ: {obj_file}")
 
+# Makes POST request to the webhook with the OBJ file
 with open(obj_file, "rb") as f:
     requests.post(
-        "https://narwhjorl.app.n8n.cloud/webhook/process-scan",
+        WEBHOOK_URL,
         files={"file": f}
     )
